c91c7e934a6cf53ffca9bb3e220d6d747dfd4c79
Kelly Cook <kellyrcook1980@gmail.com>
2021-04-23 10:53:37 -0600

    add utils and pages, work on api.js

---
+++ src/utils/API.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+export default {
+    getRandomEmployees: function() {
+        return axios.get("https://randomuser.me/api/?results=50")
+    }
+    getEmployeeSearch: function(nationality){
+        return axios.get("https://randomuser.me/api/?nat=" + nationality + "&results=20")
+    }
+
+}
